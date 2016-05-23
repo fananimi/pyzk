ded0d7bfd79b1eeb45b31e7e97c622d723f5a914
Fanani M. Ihsan <fanani.mi@gmail.com>
2016-05-23 19:49:36 +0700

    add  function

--- test.py
+++ test.py
@@ -8,7 +8,15 @@ zk = zk.ZK('192.168.1.201')
 status, message = zk.connect()
 if status:
     print 'Firmware Version: : {}'.format(zk.get_firmware_version())
-    zk.restart()
+    users = zk.get_users()
+    if users:
+        for uid in users:        
+            if users[uid][2] == 14:
+                level = 'Admin'
+            else:
+                level = 'User'
+            print "[UID %d]: ID: %s, Name: %s, Level: %s, Password: %s" % ( uid, users[uid][0], users[uid][1], level, users[uid][3]  )
+
     print zk.disconnect()
 else:
     print message
