1fbd93967d4e04398aacc5c574bb65eb4cacac61
Fanani M. Ihsan <fanani.mi@gmail.com>
2016-05-26 23:04:37 +0700

    add installer

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyzk',
+    version='0.1',
+    description='The unofficial library of zksoftware the fingerprint attendance machine.',
+    url='https://github.com/fananimi/pyzk',
+    author='Fanani M. Ihsan',
+    author_email='fanani.mi@gmail.com',
+    license='LICENSE.txt',
+    packages=['zk'],
+    zip_safe=False
+)
