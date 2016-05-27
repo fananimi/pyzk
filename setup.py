64b2ad38806b9127d1dd8129a59b57e735913cc9
Fanani M. Ihsan <fanani.mi@gmail.com>
2016-05-27 08:24:56 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,20 @@ from setuptools import setup
 setup(
     name='pyzk',
     version='0.1',
-    description='The unofficial library of zksoftware the fingerprint attendance machine.',
+    description='a unofficial library of zksoftware the fingerprint attendance machine.',
     url='https://github.com/fananimi/pyzk',
     author='Fanani M. Ihsan',
     author_email='fanani.mi@gmail.com',
     license='LICENSE.txt',
     packages=['zk'],
+    keywords = [
+        'zksoftware',
+        'pyzk',
+        'zk',
+        'attendance machine',
+        'fingerprint',
+        'biometrics',
+        'security'
+    ],
     zip_safe=False
 )
