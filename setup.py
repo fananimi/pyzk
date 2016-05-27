af9e590ccb36cf2bdad8a7d878d9f56489b1bff0
Fanani M. Ihsan <fanani.mi@gmail.com>
2016-05-27 08:48:58 +0700

    typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pyzk',
     version='0.1',
-    description='a unofficial library of zksoftware the fingerprint attendance machine.',
+    description='an unofficial library of zksoftware the fingerprint attendance machine.',
     url='https://github.com/fananimi/pyzk',
     author='Fanani M. Ihsan',
     author_email='fanani.mi@gmail.com',
