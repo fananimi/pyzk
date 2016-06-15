410ef01cbb5b96a0ab7b5ef8535a9f0dcd794b73
Fanani M. Ihsan <fanani.mi@gmail.com>
2016-06-15 19:41:59 +0700

    fixing method clear_data

--- zk/base.py
+++ zk/base.py
@@ -440,8 +440,7 @@ class ZK(object):
 
         cmd_response = self.__send_command(command, command_string, checksum, session_id, reply_id, response_size)
         if cmd_response.get('status'):
-            serialnumber = self.__data_recv[8:].split('=')[-1].strip('\x00|\x01\x10x')
-            return serialnumber
+            return True
         else:
             raise ZKErrorResponse("Invalid response")
 
